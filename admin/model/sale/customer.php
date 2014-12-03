70bad95111b5ab53fecbdd46c4a336a6dcdc906d
John LeVan <john.levan@1099-etc.com>
2014-12-03 09:49:31 -0600

    Fixing a bug with array to string conversion

--- admin/model/sale/customer.php
+++ admin/model/sale/customer.php
@@ -541,7 +541,7 @@ class ModelSaleCustomer extends Model {
       $payroll_states = implode(',',$payroll_states);
     }
     else {
-      $payroll_states = implode(',',$payroll_states);
+      $payroll_states = $payroll_states;
     }      
     $this->db->query("UPDATE " . DB_PREFIX . "customer SET payroll_states = '" . $payroll_states . "' WHERE customer_id = '" . (int)$customer_id . "'");
   }
